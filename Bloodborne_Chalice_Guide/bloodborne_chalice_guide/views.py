a214d7487cca1fd8f9cf4148d1039bb6695a4ec3
BHouwens <byron@siyavula.com>
2015-05-06 15:48:22 +0200

    Changes to jinja view

--- Bloodborne_Chalice_Guide/bloodborne_chalice_guide/views.py
+++ Bloodborne_Chalice_Guide/bloodborne_chalice_guide/views.py
@@ -15,6 +15,27 @@ def home(request):
 	with open(path, "rb") as obj:
 		context = reader(obj)
 
+	'''
+	  Pull first value out and stick it in a variable.
+	  For loop is the only way to pull first value from a dict.
+	'''
+	first_entry = context[0]
+	first_value = []
+
+	for value in first_entry.values():
+		first_value = value
+
+	'''
+	  Add headings from first_value into a new variable
+	'''
+	headings = []
+
+	for entry in first_value:
+		for key in entry.keys():
+			headings.append(key)
+
+
 	info["chalices"] = context
+	info["headings"] = headings
 
 	return info
