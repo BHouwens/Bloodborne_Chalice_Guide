5e04d494e72b45dd8253d19aaf91b70f16ff417c
BHouwens <zhion360@gmail.com>
2015-09-23 12:10:38 +0200

    Changes to app.py

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 import csv
 
 app = Flask(__name__)
+data_path = 'data.csv'
+
+def reader(path):
+	reader = csv.DictReader(path, delimiter = ",")
+
+	for row in reader:
+		# do something with the csv row
 
 @app.route('/')
 def home():
+	with open(data_path, 'rb') as doc:
+		data = reader(doc)
+
 	return render_template('index.html')
 
 
