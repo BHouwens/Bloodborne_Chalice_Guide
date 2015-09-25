1539e1aebd71f361600d53492bc4a6d6bf20d70f
BHouwens <zhion360@gmail.com>
2015-09-25 08:31:11 +0200

    Adding in basic GUI

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ data_path = 'data/data.csv'
 @app.route('/')
 def home():
 	raw_data = Chalice_Builder(data_path)
+	chalices = []
 
-	return render_template('index.html')
+
+	for item in raw_data.chalices:
+		chalices.append(item['name'])
+
+
+	return render_template('index.html',
+		names=chalices,
+		raw=['yebo'])
 
 
 if __name__ == "__main__":
